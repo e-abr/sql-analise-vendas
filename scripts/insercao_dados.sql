d03c549aeee98b8867c7060886bbcda4efc44b29
Ewerton Abrantes <11835212+e-abr@users.noreply.github.com>
2026-05-25 21:32:08 -0300

    Update insercao_dados.sql

--- scripts/insercao_dados.sql
+++ scripts/insercao_dados.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO vendas_agricolas
+(produto, quantidade, preco_unitario, data_venda)
+VALUES
+('Milho', 100, 45.50, '2026-01-10'),
+('Soja', 80, 120.00, '2026-01-15'),
+('Feijão', 60, 95.90, '2026-01-20');
