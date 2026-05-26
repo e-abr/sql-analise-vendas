99af896bcf62568f010c66202a2b694e04fe4ac1
Ewerton Abrantes <11835212+e-abr@users.noreply.github.com>
2026-05-25 21:33:43 -0300

    Update procedimento.sql

--- scripts/procedimento.sql
+++ scripts/procedimento.sql
@@ -1,1 +1,13 @@
+DELIMITER //
 
+CREATE PROCEDURE relatorio_vendas()
+BEGIN
+    SELECT
+        produto,
+        SUM(quantidade) AS total_quantidade,
+        SUM(quantidade * preco_unitario) AS faturamento_total
+    FROM vendas_agricolas
+    GROUP BY produto;
+END //
+
+DELIMITER ;
